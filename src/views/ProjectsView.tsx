75df8504ee71c4bbd9650f9a4b2826d585501137
Muhammad Fikri Ihsan <mfikriihsan03@gmail.com>
2022-11-16 13:38:13 +0700

    create sample project box

--- src/views/ProjectsView.tsx
+++ src/views/ProjectsView.tsx
@@ -1,5 +1,8 @@
 const ProjectsView = () => {
-    return ( <div className="min-h-[80vh] bg-red-500 mb-4"></div> );
+    return ( 
+    <div className="min-h-[80vh] flex items-center justify-center">
+        <div className="w-[100px] h-[100px] bg-white border-[#cb3b8a] border-2 pink-shadow"></div>
+    </div> );
 }
  
 export default ProjectsView;
