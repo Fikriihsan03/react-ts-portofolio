87e7d76f55d3f8df382649e4daa875350defc788
Muhammad Fikri Ihsan <mfikriihsan03@gmail.com>
2022-11-18 21:34:49 +0700

    feat: create project card component

---
+++ src/assets/projectsData.js
@@ -0,0 +1,18 @@
+export const projectsData =[
+    {
+        title:"Vue TypeTest",
+        description:"an application to test your typing speed on keyboard",
+        techStack:["Vuejs","css"],
+        isOnline:true,
+        webLink:"https://vue-typetest.netlify.app/",
+        sourceLink:"https://github.com/Fikriihsan03/vue-typetest"
+    },
+    {
+        title:"Face Recognition App",
+        description:"is a face recognition application from images entered via URL input, to be able to access it you must register and sign in first",
+        techStack:["Reactjs","css"],
+        isOnline:true,
+        webLink:"https://fikri-app1.herokuapp.com/",
+        sourceLink:"https://github.com/Fikriihsan03/faceRecognitionApp"
+    }
+]
--- src/views/ProjectsView.tsx
+++ src/views/ProjectsView.tsx
@@ -1,8 +1,40 @@
+import Title from "../components/Title/Title";
+import { projectsData } from "../assets/projectsData";
 const ProjectsView = () => {
-    return ( 
-    <div className="min-h-[80vh] flex items-center justify-center">
-        <div className="w-[100px] h-[100px] bg-white border-[#cb3b8a] border-2 pink-shadow"></div>
-    </div> );
-}
- 
+  return (
+    <div className="min-h-[85vh]">
+      <Title text="Projects" />
+      <div className="grid grid-cols-3 gap-4 m-4 ">
+        {projectsData.map((value, i) => {
+          return (
+            <div
+              key={i}
+              className="p-8 bg-white border-[#cb3b8a] border-2 pink-shadow cursor-default flex flex-col items-stretch "
+            >
+              <h1 className="font-bold text-2xl mb-4">{value.title}</h1>
+              <p className="poppins">{value.description}</p>
+              <div className="h-full flex">
+                <div className="gap-8 flex self-end mt-4">
+                  <button className="hover:bg-black  flex justify-center items-center gap-4 text-xl pink-button">
+                    <a href={value.sourceLink} target="_blank" rel="noreferrer">
+                      Source
+                    </a>
+                  </button>
+                  {value.isOnline ? (
+                    <button className="hover:bg-black flex justify-center items-center gap-4 text-xl pink-button">
+                      <a href={value.webLink} target="_blank" rel="noreferrer">
+                        Live Web
+                      </a>
+                    </button>
+                  ) : null}
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
 export default ProjectsView;
